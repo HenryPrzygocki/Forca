9f49c69f958043c65d116b7627042d5a96a38b77
Henry Przygocki <mateusprzygocki@gmail.com>
2022-04-08 11:19:42 -0300

    Update Forca.py

--- Forca.py
+++ Forca.py
@@ -12,9 +12,9 @@ w = []
 print(' '.join(map(str, d)))
 rep = 1
 erros = 0
-max = 5
-while(erros < max):
-    for i in range(0,max):
+errrmax = 5
+while(erros < errrmax):
+    for i in range(0,errrmax):
         w.insert(i,input("Letra: ").lower()[0])
         if(w.count(w[i]) > 1):
             w.pop(i)
@@ -33,7 +33,7 @@ while(erros < max):
         if(w[i] in palavra):
             pos = [j for j, x in enumerate(palavra) if x == w[i]]
             print(chr(27) + "[2J")
-            for x in range(0,len(pos)):
+            for x, item in enumerate(pos):
                 d[pos[x]]=w[i]
             if("".join(map(str, d))== palavra):
                 print("Voce acertou! A palavra era: ")
@@ -42,8 +42,8 @@ while(erros < max):
         else:
             erros += 1
             print(chr(27) + "[2J")
-            print("Não possui letra " ,w[i]," | ","Vidas restantes: ", max-erros)
-            if(erros == max):
+            print("Não possui letra " ,w[i]," | ","Vidas restantes: ", errrmax-erros)
+            if(erros == errrmax):
                 print("Perdeu! A palavra era: ")
                 print(palavra)
                 exit()
